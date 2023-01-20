3c139aa9ce6bbfa0ff9d5b96ecfb9673c8c0065c
Arjit Goyal <75082382+iamarjitgoyal@users.noreply.github.com>
2023-01-20 22:51:52 +0530

    Time: 50 ms (86.86%), Space: 22 MB (80.95%) - LeetHub

--- 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
+++ 0491-non-decreasing-subsequences/0491-non-decreasing-subsequences.cpp
@@ -21,3 +21,23 @@ private:
         }
     }
 };
+
+
+
+// res : a set of vectors that will store all the possible non-decreasing subsequences of the given array with at least two elements.
+
+// temp : a vector that will temporarily store the current subsequence.
+
+// nums : the input array
+
+// start : the starting index of the array to explore.
+
+// First, the function checks if the current temp vector has more than one element, if yes then it is inserted into the set.
+
+// Then the function enters a loop that starts from the start index and goes till the end of the array.
+
+// In the loop, if the temp vector is empty or the current element of the array is greater than or equal to the last element of the temp vector, the current element is added to the temp vector and the helper function is called recursively with the updated temp vector, the same array and the next index i+1.
+
+// After the recursive call, the last element of the temp vector is removed so that the temp vector is ready for the next exploration.
+
+// Finally, the set is converted to a vector and returned.
