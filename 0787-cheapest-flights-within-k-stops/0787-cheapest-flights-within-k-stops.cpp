be727822cb5585edc00c0dd98a415d7f75ce2aae
Arjit Goyal <75082382+iamarjitgoyal@users.noreply.github.com>
2023-01-26 14:36:30 +0530

    Time: 56 ms (27.63%), Space: 18.2 MB (11.18%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
-    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
+    int dp[10001][101];
+	
+    int dfs(vector<vector<pair<int, int>>>& graph, int src, int dst, int k) {
+        if (src == dst) return 0;
+        if (k <= -1) return INT_MAX;
         
-        vector<vector< pair<int,int> >> graph(n);
-        for( auto edge: flights ){
-            graph[edge[0]].push_back( {edge[1],edge[2]} );
+        if (dp[src][k] != -1) return dp[src][k];
+        
+        int ans = INT_MAX;
+        for (auto child : graph[src]) {
+            int res = dfs(graph, child.first, dst, k-1);
+            if (res != INT_MAX)
+                ans = min(ans, child.second +res);
         }
         
-        vector<int> dist(n,pow(10,4)*n) , prev(n);
-        dist[src] = 0;
-        prev = dist;
-        k++;
-        while( k-- ){
-            for( int i = 0 ; i < n ; i++ ){
-                for( int j = 0; j < graph[i].size(); j++ ){
-                    int index = graph[i][j].first;
-                    int value = graph[i][j].second;
-                    // at x'th step there can be more than one updates
-                    // so we compare the update value, of the index 
-                    // gettting updated 
-                    if( dist[ index ] > prev[i] + value ){
-                        dist[ index ] = prev[i] + value;
-                    }
-                }
-            }
-            prev = dist;
+        return dp[src][k] = ans;
+    }
+    
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
+        memset(dp, -1, sizeof dp);
+        // Build the graph
+        vector<vector<pair<int, int>>> graph(n);
+        for (auto vec : flights) {
+            graph[vec[0]].emplace_back(make_pair(vec[1], vec[2]));
         }
-        if( prev[dst] == pow(10,4)*n ){
+        int ans = dfs(graph, src, dst, K);  // or make the dfs call
+        if (ans == INT_MAX)
             return -1;
-        }
-        return prev[dst];
+        return ans;
     }
 };
