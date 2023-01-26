88b756d569689723d0b0fe0976856357e5578794
Arjit Goyal <75082382+iamarjitgoyal@users.noreply.github.com>
2023-01-26 14:34:51 +0530

    Time: 90 ms (15.68%), Space: 13.7 MB (39.70%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,17 +1,34 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        vector<int> dist(n, INT_MAX);
-        dist[src] = 0;
-        for (int i = 0; i <= k; i++) {
-            vector<int> temp(dist);
-            for (auto e : flights) {
-                int u = e[0], v = e[1], w = e[2];
-                if (dist[u] == INT_MAX) continue;
-                temp[v] = min(temp[v], dist[u] + w);
-            }
-            dist = temp;
+        
+        vector<vector< pair<int,int> >> graph(n);
+        for( auto edge: flights ){
+            graph[edge[0]].push_back( {edge[1],edge[2]} );
         }
-        return dist[dst] == INT_MAX ? -1 : dist[dst];
+        
+        vector<int> dist(n,pow(10,4)*n) , prev(n);
+        dist[src] = 0;
+        prev = dist;
+        k++;
+        while( k-- ){
+            for( int i = 0 ; i < n ; i++ ){
+                for( int j = 0; j < graph[i].size(); j++ ){
+                    int index = graph[i][j].first;
+                    int value = graph[i][j].second;
+                    // at x'th step there can be more than one updates
+                    // so we compare the update value, of the index 
+                    // gettting updated 
+                    if( dist[ index ] > prev[i] + value ){
+                        dist[ index ] = prev[i] + value;
+                    }
+                }
+            }
+            prev = dist;
+        }
+        if( prev[dst] == pow(10,4)*n ){
+            return -1;
+        }
+        return prev[dst];
     }
 };
