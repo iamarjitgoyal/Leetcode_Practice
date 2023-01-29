86e15f72fa86b49767e33c3caa32552ac4ba6187
Arjit Goyal <75082382+iamarjitgoyal@users.noreply.github.com>
2023-01-29 22:59:29 +0530

    Time: 145 ms (80.51%), Space: 23.7 MB (60.81%) - LeetHub

---
+++ 1508-range-sum-of-sorted-subarray-sums/1508-range-sum-of-sorted-subarray-sums.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int rangeSum(vector<int>& nums, int n, int left, int right) {
+        vector<int> sums;
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+            for (int j = i; j < n; j++) {
+                sum += nums[j];
+                sums.push_back(sum);
+            }
+        }
+        sort(sums.begin(), sums.end());
+        int result = 0;
+        for (int i = left - 1; i < right; i++) {
+            result = (result + sums[i]) % (int)(1e9 + 7);
+        }
+        return result;
+    }
+};
