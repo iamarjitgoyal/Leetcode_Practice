d686157924721f213d8e406b60a3270249539c25
Arjit Goyal <75082382+iamarjitgoyal@users.noreply.github.com>
2023-01-29 22:53:35 +0530

    Time: 82 ms (93.91%), Space: 40.5 MB (36.64%) - LeetHub

---
+++ 0528-random-pick-with-weight/0528-random-pick-with-weight.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private:
+    vector<int> prefix_sum;
+    int total_sum;
+
+public:
+    Solution(vector<int>& w) {
+        total_sum = 0;
+        for (int weight : w) {
+            total_sum += weight;
+            prefix_sum.push_back(total_sum);
+        }
+    }
+    
+    int pickIndex() {
+        int random_num = rand() % total_sum; // generate a random number between 0 and total_sum-1
+        int left = 0, right = prefix_sum.size() - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (random_num >= prefix_sum[mid]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+};
