5b56cadc79172604899e3a4a595e29954718119d
Arjit Goyal <75082382+iamarjitgoyal@users.noreply.github.com>
2023-03-06 13:32:09 +0530

    Time: 138 ms (10.74%), Space: 27.4 MB (12.35%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -3,17 +3,20 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> mp;
         vector<vector<string>> result;
-        
         for (string s : strs) {
-            string sorted = s;
-            sort(sorted.begin(), sorted.end());
-            mp[sorted].push_back(s);
+            int count[26] = {0};
+            for (char c : s) {
+                count[c - 'a']++;
+            }
+            string hash = "";
+            for (int i = 0; i < 26; i++) {
+                hash += "#" + to_string(count[i]);
+            }
+            mp[hash].push_back(s);
         }
-        
         for (auto p : mp) {
             result.push_back(p.second);
         }
-        
         return result;
     }
 };
