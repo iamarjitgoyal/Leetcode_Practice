fb38a46d2515260787428cda81479f3acb6689a0
Arjit Goyal <75082382+iamarjitgoyal@users.noreply.github.com>
2023-04-07 21:22:58 +0530

    Time: 83 ms (43.81%), Space: 27.9 MB (70.94%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -3,33 +3,38 @@ public:
     int numEnclaves(vector<vector<int>>& grid) {
         int m = grid.size();
         int n = grid[0].size();
-        function<void(int, int)> dfs = [&](int i, int j) {
-            if (i < 0 || i >= m || j < 0 || j >= n || grid[i][j] != 1) {
-                return;
+        int count = 0;
+        queue<pair<int, int>> q;
+        auto bfs = [&](int i, int j) {
+            q.push({i, j});
+            while (!q.empty()) {
+                auto [x, y] = q.front(); q.pop();
+                if (x < 0 || x >= m || y < 0 || y >= n || grid[x][y] != 1) {
+                    continue;
+                }
+                grid[x][y] = 2;
+                q.push({x+1, y});
+                q.push({x-1, y});
+                q.push({x, y+1});
+                q.push({x, y-1});
             }
-            grid[i][j] = 2;
-            dfs(i+1, j);
-            dfs(i-1, j);
-            dfs(i, j+1);
-            dfs(i, j-1);
         };
         for (int j = 0; j < n; j++) {
             if (grid[0][j] == 1) {
-                dfs(0, j);
+                bfs(0, j);
             }
             if (grid[m-1][j] == 1) {
-                dfs(m-1, j);
+                bfs(m-1, j);
             }
         }
         for (int i = 0; i < m; i++) {
             if (grid[i][0] == 1) {
-                dfs(i, 0);
+                bfs(i, 0);
             }
             if (grid[i][n-1] == 1) {
-                dfs(i, n-1);
+                bfs(i, n-1);
             }
         }
-        int count = 0;
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == 1) {
