04b9c51ea865d670d1aafc992bd95994e37d3008
Arjit Goyal <75082382+iamarjitgoyal@users.noreply.github.com>
2023-05-08 11:12:54 +0530

    Time: 18 ms (27.79%), Space: 11.3 MB (64.67%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -6,7 +6,6 @@ public:
         
         for (int i = 0; i < n; i++) {
             sum += mat[i][i];
-            
             if (i != n-1-i) {
                 sum += mat[i][n-1-i];
             }
