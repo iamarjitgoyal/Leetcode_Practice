30c08737d0dfe7fefcc677b1b2c4aa87ca0972de
Arjit Goyal <75082382+iamarjitgoyal@users.noreply.github.com>
2023-07-11 21:47:30 +0530

    Time: 5 ms (68.67%), Space: 15.1 MB (14.91%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.cpp
@@ -5,21 +5,20 @@ public:
     void dfs(TreeNode* node, TreeNode* parent) {
         if (!node) return;
         
-        if (!parent)
-            parent = node;
-        else
-            parentMap[node] = parent;
+        parentMap[node] = parent;
         
         dfs(node->left, node);
         dfs(node->right, node);
     }
     
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
-        queue<TreeNode*> queue;
-        set<TreeNode*> visited;
-        
         dfs(root, nullptr);
+        
+        queue<TreeNode*> queue;
         queue.push(target);
+        
+        unordered_set<TreeNode*> visited;
+        visited.insert(target);
         
         int distance = 0;
         vector<int> result;
@@ -27,33 +26,29 @@ public:
         while (!queue.empty()) {
             int levelSize = queue.size();
             
-            if (distance > k)
-                break;
+            if (distance == k) {
+                while (levelSize--) {
+                    TreeNode* node = queue.front();
+                    queue.pop();
+                    result.push_back(node->val);
+                }
+                return result;
+            }
             
             while (levelSize--) {
                 TreeNode* node = queue.front();
                 queue.pop();
                 
-                if (node->left && !visited.count(node->left)) {
+                if (node->left && visited.insert(node->left).second)
                     queue.push(node->left);
-                    visited.insert(node->left);
-                }
                 
-                if (node->right && !visited.count(node->right)) {
+                if (node->right && visited.insert(node->right).second)
                     queue.push(node->right);
-                    visited.insert(node->right);
-                }
                 
-                if (parentMap.count(node) && !visited.count(parentMap[node])) {
-                    queue.push(parentMap[node]);
-                    visited.insert(parentMap[node]);
-                }
+                TreeNode* parent = parentMap[node];
                 
-                visited.insert(node);
-                
-                if (distance == k) {
-                    result.push_back(node->val);
-                }
+                if (parent && visited.insert(parent).second)
+                    queue.push(parent);
             }
             
             distance++;
