e8c243dbead9b8b363009075a7931b4cb116a829
Arjit Goyal <75082382+iamarjitgoyal@users.noreply.github.com>
2023-07-26 23:38:21 +0530

    Time: 291 ms (85.90%), Space: 101.5 MB (26.33%) - LeetHub

---
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cpp
@@ -0,0 +1,33 @@
+#include <vector>
+#include <cmath>
+
+class Solution {
+public:
+    int minSpeedOnTime(std::vector<int>& dist, double hour) {
+        int n = dist.size();
+        int left = 1;
+        int right = 10000000;
+        
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            double total_time = 0;
+            
+            for (int i = 0; i < n - 1; i++) {
+                total_time += std::ceil(static_cast<double>(dist[i]) / mid); 
+            }
+            
+            total_time += static_cast<double>(dist[n - 1]) / mid; 
+            
+            if (total_time <= hour) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        if (left > 10000000) {
+            return -1;
+        }
+        return left;
+    }
+};
